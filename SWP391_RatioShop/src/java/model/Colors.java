0ce68b1239f996e0374b6d885b609d8b02772fc1
ducthien-1 <144603170+ducthien-1@users.noreply.github.com>
2024-02-03 15:07:24 +0700

    Add Colors.java

---
+++ SWP391_RatioShop/src/java/model/Colors.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author admin
+ */
+public class Colors {
+    private int colorId;
+    private String color;
+
+    public Colors() {
+    }
+
+    public Colors(int colorId, String color) {
+        this.colorId = colorId;
+        this.color = color;
+    }
+
+    public int getColorId() {
+        return colorId;
+    }
+
+    public void setColorId(int colorId) {
+        this.colorId = colorId;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+}
