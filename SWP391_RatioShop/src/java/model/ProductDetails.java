31802f1129b249520d7b9fdc2af8e0faa368901c
ducthien-1 <144603170+ducthien-1@users.noreply.github.com>
2024-02-03 15:06:50 +0700

    Add ProductDetails.java

---
+++ SWP391_RatioShop/src/java/model/ProductDetails.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author admin
+ */
+public class ProductDetails {
+    private int productId;
+    private int sizeId;
+    private int colorId;
+    private int quantity;
+
+    public ProductDetails() {
+    }
+
+    public ProductDetails(int productId, int sizeId, int colorId, int quantity) {
+        this.productId = productId;
+        this.sizeId = sizeId;
+        this.colorId = colorId;
+        this.quantity = quantity;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getSizeId() {
+        return sizeId;
+    }
+
+    public void setSizeId(int sizeId) {
+        this.sizeId = sizeId;
+    }
+
+    public int getColorId() {
+        return colorId;
+    }
+
+    public void setColorId(int colorId) {
+        this.colorId = colorId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+}
