fb221fcc5e8840965ac60dc61098b65b25045ba1
ducthien-1 <144603170+ducthien-1@users.noreply.github.com>
2024-02-26 20:13:51 +0700

    Update ProductSizeDAO.java

--- SWP391_RatioShop/src/java/DAO/ProductSizeDAO.java
+++ SWP391_RatioShop/src/java/DAO/ProductSizeDAO.java
@@ -2,34 +2,166 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package DAO;
+package dal;
 
-import model.ProductSize;
+import Model.Color;
+import Model.Product;
+import Model.ProductDetail;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import dbcontext.DBContext;
+import java.util.Map;
 
-public class ProductSizeDAO extends DBContext {
+public class ProductDetailDAO extends DBContext {
 
-    public List<ProductSize> getProductSizeByProductId(int productId) {
-        String sql = "select * from ProductSize where productId = " + productId;
+    public ProductDetail getProductDetailById(int detailId) {
+        String sql = "select * from ProductDetails where id =  " + detailId;
         ProductDAO pDAO = new ProductDAO();
         SizeDAO sDAO = new SizeDAO();
-        List<ProductSize> list = new ArrayList<>();
+        ColorDAO cDAO = new ColorDAO();
         try {
             PreparedStatement st = connection.prepareStatement(sql);
             ResultSet rs = st.executeQuery();
             while (rs.next()) {
-                ProductSize ps = new ProductSize();
+                ProductDetail ps = new ProductDetail();
+                ps.setId(rs.getInt("id"));
                 ps.setProduct(pDAO.getProductById(rs.getInt("productId")));
                 ps.setSize(sDAO.getSizeById(rs.getInt("sizeId")));
                 ps.setQuantity(rs.getInt("quantity"));
+                ps.setColor(cDAO.getColorById(rs.getInt("colorId")));
+                ps.setProductImage(rs.getString("productImage"));
+                return ps;
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public List<ProductDetail> getProductDetailsByProductId(int productId) {
+        String sql = "select * from ProductDetails where productId = " + productId + " order by colorId";
+        ProductDAO pDAO = new ProductDAO();
+        SizeDAO sDAO = new SizeDAO();
+        ColorDAO cDAO = new ColorDAO();
+        List<ProductDetail> list = new ArrayList<>();
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                ProductDetail ps = new ProductDetail();
+                ps.setId(rs.getInt("id"));
+                ps.setProduct(pDAO.getProductById(rs.getInt("productId")));
+                ps.setSize(sDAO.getSizeById(rs.getInt("sizeId")));
+                ps.setQuantity(rs.getInt("quantity"));
+                ps.setColor(cDAO.getColorById(rs.getInt("colorId")));
+                ps.setProductImage(rs.getString("productImage"));
                 list.add(ps);
             }
         } catch (Exception e) {
         }
         return list;
     }
+
+    public static void main(String[] args) {
+        ProductDetailDAO pDAO = new ProductDetailDAO();
+        List<ProductDetail> l = pDAO.getProductDetailsByProductId(2);
+        List<Color> l2 = new ColorDAO().getListColorsByProductId(2);
+
+//        for (Color c : l2) {
+//            for (ProductDetail p : l) {
+//                if (c.getColorId() == p.getColor().getColorId()) {
+//                    System.out.println(c.getColor() + " - " + p.getSize().getSize());
+//                }
+//            }
+//        }
+//        Map<Integer, String> map = pDAO.getProductImagesOfProductById(1);
+//        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+//            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+//        }
+        System.out.println(pDAO.getQuantityOfProductById(20));
+    }
+
+    public Map<Integer, String> getProductImagesOfProductById(int productId) {
+        String sql = "select distinct colorId, productImage from ProductDetails\n"
+                + "where productId = ? order by colorId";
+        Map<Integer, String> map = new HashMap();
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, productId);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                if (rs.getString("productImage") != null) {
+                    map.put(rs.getInt("colorId"), rs.getString("productImage"));
+                }
+            }
+        } catch (Exception e) {
+        }
+        return map;
+    }
+
+    public void updateProductDetails(int productDetailId, int proId, int sizeId, int colorId, int quantity, String proImage) {
+        String query = "UPDATE [dbo].[ProductDetails]\n"
+                + "   SET [productId] = ?\n"
+                + "      ,[sizeId] = ?\n"
+                + "      ,[colorId] = ?\n"
+                + "      ,[quantity] = ?\n"
+                + "      ,[productImage] = ?\n"
+                + " WHERE id = ?";
+        try {
+            PreparedStatement st = connection.prepareCall(query);
+            st.setInt(1, proId);
+            st.setInt(2, sizeId);
+            st.setInt(3, colorId);
+            st.setInt(4, quantity);
+            st.setString(5, proImage);
+            st.setInt(6, productDetailId);
+            st.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public void insertProductDetails(int proId, int sizeId, int colorId, int quantity, String proImage) {
+        String sql = "INSERT INTO [dbo].[ProductDetails]\n"
+                + "           ([productId]\n"
+                + "           ,[sizeId]\n"
+                + "           ,[colorId]\n"
+                + "           ,[quantity]\n"
+                + "           ,[productImage])\n"
+                + "     VALUES\n"
+                + "           (?\n"
+                + "           ,?\n"
+                + "           ,?\n"
+                + "           ,?\n"
+                + "           ,?)";
+
+        try {
+            PreparedStatement st = connection.prepareCall(sql);
+            st.setInt(1, proId);
+            st.setInt(2, sizeId);
+            st.setInt(3, colorId);
+            st.setInt(4, quantity);
+            st.setString(5, proImage);
+            st.executeUpdate();
+        } catch (Exception e) {
+        }
+
+    }
+
+    public int getQuantityOfProductById(int productId) {
+        String sql = "select sum(quantity) as [sum] from ProductDetails where productId = ?";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, productId);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("sum");
+            }
+        } catch (SQLException e) {
+        }
+        return 0;
+    }
+
 }
