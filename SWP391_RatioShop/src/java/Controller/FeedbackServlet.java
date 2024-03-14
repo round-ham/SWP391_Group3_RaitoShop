91ec3101acb10dd2e72672a7644abf6ff736f53e
ducthien-1 <144603170+ducthien-1@users.noreply.github.com>
2024-03-14 14:47:22 +0700

    add feedback controller

---
+++ SWP391_RatioShop/src/java/Controller/FeedbackServlet.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller.feedback;
+
+import Model.Accounts;
+import dal.OrderDAO;
+import dal.ProductDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author admin
+ */
+@WebServlet(name = "FeedbackServlet", urlPatterns = {"/feedback"})
+public class FeedbackServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String productIdraw = request.getParameter("productId");
+        try {
+            int productId = Integer.parseInt(productIdraw);
+            ProductDAO daoP = new ProductDAO();
+            request.setAttribute("p", daoP.getProductById(productId));
+            request.getRequestDispatcher("writefeedback.jsp").forward(request, response);
+        } catch (Exception e) {
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        String productIdRaw= request.getParameter("productId");
+        String ratingRaw = request.getParameter("rating");
+        String feedbackDescription = request.getParameter("desFeedback");
+        try {
+            OrderDAO daoO = new OrderDAO();
+            int productId = Integer.parseInt(productIdRaw);
+            int rating = Integer.parseInt(ratingRaw);
+            Accounts customer = (Accounts) request.getSession().getAttribute("account");
+            daoO.addFeedBack(productId, feedbackDescription, customer.getAccountId(), rating);
+            response.sendRedirect("productdetails?id="+productId);
+        } catch (Exception e) {
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
