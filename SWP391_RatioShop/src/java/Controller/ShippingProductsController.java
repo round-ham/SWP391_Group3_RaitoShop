56da68eb4c9c58432b80c5d9174a1e7f80ba088e
Nguyễn Tiến Tú <120471719+MjVarro@users.noreply.github.com>
2024-03-19 16:33:32 +0700

    Delivery Management

---
+++ SWP391_RatioShop/src/java/Controller/ShippingProductsController.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import Model.Accounts;
+import dal.OrderDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Owwl
+ */
+public class ShippingProductsController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        OrderDAO dao = new OrderDAO();
+        request.setAttribute("availableList", dao.getAvailableShippingOrder());
+       request.getRequestDispatcher("ShippingProducts.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       try
+       {
+        int id= Integer.parseInt(request.getParameter("id"));
+       Accounts account = (Accounts)request.getSession().getAttribute("account");
+       int shipperId  = account.getAccountId();
+       OrderDAO dao = new OrderDAO();
+       dao.SetShipping(id, shipperId);
+       response.sendRedirect(request.getContextPath()+"/shiplist?success");
+       }
+       catch(Exception ex)
+       {
+         response.sendRedirect(request.getContextPath()+"/shiplist?fail");
+     
+       }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
