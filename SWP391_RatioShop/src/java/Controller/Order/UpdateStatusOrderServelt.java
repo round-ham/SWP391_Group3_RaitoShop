8ca10b1c2ab2169cd560f1554283280fd4c4c2bc
ducthien-1 <144603170+ducthien-1@users.noreply.github.com>
2024-03-21 13:08:11 +0700

    update order

--- SWP391_RatioShop/src/java/Controller/Order/UpdateStatusOrderServelt.java
+++ SWP391_RatioShop/src/java/Controller/Order/UpdateStatusOrderServelt.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+
 package Controller.Order;
 
 import dal.OrderDAO;
@@ -17,35 +18,30 @@ import jakarta.servlet.http.HttpServletResponse;
  *
  * @author ADMIN
  */
-@WebServlet(name = "UpdateStatusOrderServelt", urlPatterns = {"/update-status-order"})
+@WebServlet(name="UpdateStatusOrderServelt", urlPatterns={"/update-status-order"})
 public class UpdateStatusOrderServelt extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String previousURL =request.getHeader("referer");
         String status = request.getParameter("status");
         String orderId = request.getParameter("orderId");
         OrderDAO daoO = new OrderDAO();
         daoO.updateStatusOrder(orderId, status);
-        System.out.println("================================TEST");
-        System.out.println(previousURL);
-        response.sendRedirect("manage-orders");
-    }
+        String previousUrl = request.getParameter("previousUrl");
+        response.sendRedirect(previousUrl);
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -53,13 +49,12 @@ public class UpdateStatusOrderServelt extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
-    }
+    } 
 
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -67,13 +62,12 @@ public class UpdateStatusOrderServelt extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override
